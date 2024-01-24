bc6bded2628c3c1070cef36d7b68eedae6ce2d52
picklesniff <ivashkina.vv@gmail.com>
2024-01-24 10:17:03 +0800

    Setting the app
    1. Created app with Vite.
    2. Added seedsColors.js, Palette.js.
    3. Passing the palette by array number [4] to the App component.
    4. Palette succeeded to pass seedsColor properties.

---
+++ src/Palette.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Palette() {
+  return (
+    <div className="Palette">
+  
+      <div className="Palette-colors">Palette</div>
+      
+    </div>
+  );
+}
+export default Palette;
