d8cfd549fe72240f8a8e6656d40d635d522b98c6
picklesniff <ivashkina.vv@gmail.com>
2024-01-24 11:39:01 +0800

    Displaying color boxes
    1. Added ColorBox component.
    2. Fetching props from Palette and passing to ColorBox structure to display name, background color ++ key by unique color name.
    3. Added css to adjust view params

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import ColorBox from "./ColorBox";
+import './Palette.css'
 
-function Palette() {
+function Palette ({ colors }) {
+  const colorBoxes = colors.map(color => (
+    <ColorBox key={color.name} background={color.color} name={color.name} />
+  ));
+
   return (
     <div className="Palette">
-  
-      <div className="Palette-colors">Palette</div>
-      
+      <div className="Palette-colors">
+      {colorBoxes}
+      </div>
     </div>
   );
 }
