6e88db6f493a1d35d2f313812dbebb093ca3142c
picklesniff <ivashkina.vv@gmail.com>
2024-01-25 11:14:52 +0800

    Slider styling
    1. Created css file for Slider comp
    2. Changed styling

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -2,32 +2,32 @@ import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import './Slider.css'
 import "./Palette.css";
 
-function Palette ({ palette }) {
+function Palette({ palette }) {
   const [sliderValue, setSliderValue] = useState(500); // Initial value, adjust as needed
 
-  const colorBoxes = palette.colors[sliderValue].map(color => (
+  const colorBoxes = palette.colors[sliderValue].map((color) => (
     <ColorBox key={color.name} background={color.hex} name={color.name} />
   ));
 
-  const handleSliderChange = newValue => {
+  const handleSliderChange = (newValue) => {
     setSliderValue(newValue);
   };
 
   return (
     <div className="Palette">
-      <Slider
-        min={100}
-        max={900}
-        step={100}
-        defaultValue={sliderValue}
-        onChange={handleSliderChange}
-      />
-      <div className="Palette-colors">
-        {colorBoxes}
+        <Slider className="slider"
+          min={100}
+          max={900}
+          step={100}
+          defaultValue={sliderValue}
+          onChange={handleSliderChange}
+        />
+        <div className="Palette-colors">{colorBoxes}</div>
       </div>
-    </div>
+
   );
 }
 export default Palette;
