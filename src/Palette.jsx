21d23a729907895f1b9fc321b183ceea866eefea
picklesniff <ivashkina.vv@gmail.com>
2024-01-25 12:47:21 +0800

    Setting Navbar
    1. Added Font
    2. Separated navbar and slider to a separate file Navbar.jsx
    3. Added styles for the Navbar

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
-import './Slider.css'
+import Navbar from "./Navbar";
 import "./Palette.css";
 
 function Palette({ palette }) {
-  const [sliderValue, setSliderValue] = useState(500); // Initial value, adjust as needed
+  const [sliderValue, setSliderValue] = useState(500); 
 
   const colorBoxes = palette.colors[sliderValue].map((color) => (
     <ColorBox key={color.name} background={color.hex} name={color.name} />
@@ -18,13 +16,7 @@ function Palette({ palette }) {
 
   return (
     <div className="Palette">
-        <Slider className="slider"
-          min={100}
-          max={900}
-          step={100}
-          defaultValue={sliderValue}
-          onChange={handleSliderChange}
-        />
+        <Navbar level={sliderValue} onChange={handleSliderChange} />
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
 
