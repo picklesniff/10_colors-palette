5c5c35fefc99233f5e3e51ed5e9fc30bcba1af31
picklesniff <ivashkina.vv@gmail.com>
2024-01-26 16:04:46 +0800

    Setting Router
    1. Installed the react-router.
    2. Set route to get palette by id.
    3. Defined logic to fetch params from URL using useParams hook.
    4. Defined the wrapper function to use the id param and find palette by id

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import * as React from 'react';
-import Palette from './Palette';
-import seedsColors from './seedsColors';
+import * as React from "react";
+import { Routes, Route, useParams } from "react-router-dom";
+import Palette from "./Palette";
+import seedsColors from "./seedsColors";
 import { generatePalette } from "./colorHelpers";
 
-
 function App() {
+  const findPalette = (id) => seedsColors.find((palette) => palette.id === id);
+  const PaletteWrapper = () => {
+    const { id } = useParams();
+
+    const palette = generatePalette(findPalette(id));
+
+    return <Palette palette={palette} />;
+  };
   return (
-    <>
-     <Palette palette={generatePalette(seedsColors[4])} />
-    </>
-  )
+    <Routes>
+      <Route path="*" element={<h1>Home</h1>} />
+      <Route path="/palette/:id" element={<PaletteWrapper />}  />
+      {/* <Palette palette={generatePalette(seedsColors[4])} /> */}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
+
+
